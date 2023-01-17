febfced7642521d345d2d0dba48a0be7a1a882f9
Adam Piaseczny <cypis183@gmail.com>
2023-01-17 21:06:14 +0100

    test: loading the colorscheme is now compared to the default one

--- internal/colorscheme/colorscheme_test.go
+++ internal/colorscheme/colorscheme_test.go
@@ -5,23 +5,6 @@ import (
 	"testing"
 )
 
-// getTestColorscheme returns a test colorscheme
-func getTestColorscheme() Colorscheme {
-	return Colorscheme{
-		BgDark:   "#040612",
-		BgDarker: "#040612",
-		Text:     "#98ccdc",
-		TextDark: "#98ccdc",
-		Color1:   "#625160",
-		Color2:   "#BD4354",
-		Color3:   "#985063",
-		Color4:   "#BA9C6A",
-		Color5:   "#1E5AA6",
-		Color6:   "#C25C9F",
-		Color7:   "#98ccdc",
-	}
-}
-
 // TestColorschemeLoadNoFile if we get an error then the default colorscheme is not generated
 func TestColorschemeLoadNoFile(t *testing.T) {
 	// Create the colorscheme with a non-existent file path
@@ -40,8 +23,8 @@ func TestColorschemeLoadFile(t *testing.T) {
 	// Create the colorscheme with a non-existent file path
 	colors := New("../test/data/colorscheme.json")
 
-	// Create the test colorscheme to compare with
-	testColors := getTestColorscheme()
+	// Create the new default to compare with
+	testColors := newDefault()
 
 	if reflect.DeepEqual(colors, testColors) {
 		t.Errorf("Colorscheme not loaded correctly")
