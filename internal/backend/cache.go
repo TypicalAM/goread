92a04fb8d31f0d8ff72a2298b67627a54999085f
TypicalAM <tpam_piaseczny@protonmail.com>
2023-07-19 21:22:45 +0200

    chore: bump up the version on the User-Agent string

--- internal/backend/cache.go
+++ internal/backend/cache.go
@@ -216,7 +216,7 @@ func parseFeed(feedURL string) (*gofeed.Feed, error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("User-Agent", "goread:v1.3.0 (by /u/TypicalAM)")
+	req.Header.Set("User-Agent", "goread:v1.3.1 (by /u/TypicalAM)")
 
 	// Send the request
 	resp, err := client.Do(req)
