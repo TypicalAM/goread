397df6a23f2427386769bcfdf798772fbac6390f
TypicalAM <tpam_piaseczny@protonmail.com>
2023-07-24 01:40:17 +0200

    chore: remove the profiling

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"log"
-	"os"
-	"runtime/pprof"
-
 	"github.com/TypicalAM/goread/cmd/goread"
 )
 
@@ -13,16 +9,6 @@ var (
 )
 
 func main() {
-	f, err := os.Create("test.prof")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = pprof.StartCPUProfile(f); err != nil {
-		log.Fatal(err)
-	}
-
-	defer pprof.StopCPUProfile()
 	goread.SetVersion(version)
 	goread.Execute()
 }
